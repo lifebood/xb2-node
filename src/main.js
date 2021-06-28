d79415dd5d479e9dc61808440a4fd0c22fdba76b
lifebood <lifebood@qq.com>
2021-06-28 22:02:32 +0800

    web-server

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.write('hello~');
+  //   console.log(request.headers['user-agent']);
+  //   response.writeHead(200, {
+  //     'Content-Type': 'text/html',
+  switch (request.url) {
+    case '/':
+      response.write('hello~');
+      break;
+    case '/posts':
+      response.write('posts~');
+      break;
+    case '/signup':
+      response.write('signup~');
+      break;
+
+    default:
+      response.writeHead(404);
+      response.write('404');
+      break;
+  }
   response.end();
 });
+//   response.write(`<input />`);
+
+//   response.end();
+
 server.listen(3000, () => {
   console.log('服务已启动');
 });
