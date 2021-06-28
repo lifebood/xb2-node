a2b0b1c2733ab99fc2f246bff9b66d3b351404cd
lifebood <lifebood@qq.com>
2021-06-28 18:15:32 +0800

    0628练习

--- src/playground/index.js
+++ src/playground/index.js
@@ -34,7 +34,68 @@
 // const [f1,f2,f3]=fruits;
 // console.log(f1,f2,f3);
 
+// const fruits = ['苹果', '香蕉', '桔子'];
+// const vegetables = ['土豆', '茄子', '辣椒'];
+// const food = [...fruits, ...vegetables];
+// console.log(food);
 
+//展开操作符  ...
+// const data = {
+//   title: '小白的开发之路',
+// };
+// const author = {
+//   name: 'yy',
+// };
+// const book = {
+//   ...data,
+//   author,
+// };
+// console.log(book);
 
+//if
+// function aa(speed1) {
+//   let speed = speed1;
+//   if (speed > 120) {
+//     console.log('您已超速');
+//   } else {
+//     console.log('您没有超速');
+//   }
+// }
+// aa(120);
 
-
+//switch
+// let gear = 'D';
+// switch (gear) {
+//   case 'P':
+//     console.log('停车');
+//     break;
+//   case 'R':
+//     console.log('倒档');
+//     break;
+//   case 'D':
+//     console.log('开车');
+//     break;
+//   case 'N':
+//     console.log('空挡');
+//     break;
+// }
+//throw
+// const drive = () => {
+//   throw new Error('没油了');
+// };
+// drive();
+const getGasonLine = () => {
+  return false;
+};
+const drive = () => {
+  const gasonline = getGasonLine();
+  if (!gasonline) {
+    throw new Error('没油了');
+  }
+  console.log('呜~呜~~~~');
+};
+try {
+  drive();
+} catch (error) {
+  console.log(error.message);
+}
