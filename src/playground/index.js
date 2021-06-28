4ad8e47a3ae6f11ef48eba42f985f6231346781d
lifebood <lifebood@qq.com>
2021-06-28 16:39:26 +0800

    熟悉语言

---
+++ src/playground/index.js
@@ -0,0 +1,40 @@
+// function log(){
+//     console.log('log::');
+// }
+// const log = (message,prefix)=>{
+//     console.log(prefix,message);
+// }
+
+// log('hello~','LOG');
+// log('你好','日志');
+
+// const great=(name)=>'你好'+name;
+// const greating = great('YY');
+// console.log(greating);
+
+// const book={
+//     title:'小白兔的开发之路',
+//     toString(){
+//         return `《${this.title}》`;
+//     }
+// };
+// console.log(book.toString());
+
+// const book={
+//     title:'小白兔的开发之路',
+//     author:'yy'
+// }
+
+// const{title,author}=book;
+// const{title:title1,author:author1}=book;
+// console.log(`《${title}》${author}`);
+// console.log(`《${title1}》${author1}`);
+
+// const fruits=['苹果','香蕉','桔子'];
+// const [f1,f2,f3]=fruits;
+// console.log(f1,f2,f3);
+
+
+
+
+
