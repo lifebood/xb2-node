f31fd5c64855186b9921c5be0a7a7f3b89f0f157
lifebood <lifebood@qq.com>
2021-06-29 21:27:49 +0800

    express

--- src/main.js
+++ src/main.js
@@ -1,43 +1,41 @@
-const http = require('http');
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const prot = 3000;
 
-const server = http.createServer((request, response) => {
-  //   console.log(request.headers['user-agent']);
-  //   response.writeHead(200, {
-  //     'Content-Type': 'text/html',
-  //   switch (request.url) {
-  //     case '/':
-  //       response.write('hello~');
-  //       break;
-  //     case '/posts':
-  //       response.write('posts~');
-  //       break;
-  //     case '/signup':
-  //       response.write('signup~');
-  //       break;
-
-  //     default:
-  //       response.writeHead(404);
-  //       response.write('404');
-  //       break;
-  //   }
-
-  const data = {
-    id: 1,
-    title: '关山月',
-    content: '明月出天山,苍茫云海间',
-  };
-  const jsondata = JSON.stringify(data);
-  response.writeHead(200, {
-    'Content-Type': 'application/json;charset=utf-8',
-  });
-  response.write(jsondata);
-  response.end();
-});
-//   response.write(`<input />`);
-
-//   response.end(); curl curlip.me
-
-server.listen(3000, () => {
+app.listen(prot, () => {
   console.log('服务已启动');
 });
-//https://ninghao.net/course/8184 6282234
+app.get('/', (request, response) => {
+  response.send('你好');
+});
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山,苍茫白云间',
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶,一览众山小',
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出红花红胜火,春来江水绿如蓝',
+  },
+];
+
+app.get('/posts', (request, response) => {
+  response.send(data);
+});
+
+app.get('/posts/:postId', (req, res) => {
+  //获取内容ID
+  const { postId } = req.params;
+  //查找具体内容
+  const posts = data.filter((item) => item.id == postId);
+  //做出响应
+  res.send(posts[0]);
+});
